6a58dcf6829c73cae3eb4d109f1093419e3e1a53
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-11-08 15:05:04 +0530

    volley

--- src/pages/packages/index.jsx
+++ src/pages/packages/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import pkg from "../data"
-
+const pkg=[{
+  id:1,
+  name:"Volley",
+  setup:`implementation("com.android.volley:volley:1.1.1")`,
+  reference:"https://www.geeksforgeeks.org/making-api-calls-using-volley-library-in-android/",
+  origin:"https://google.github.io/volley/"
+}]
 const Packages = () => {
   return (
     <div>
