d0571f7ec4f8e7270d2bc55478778fad95d52b43
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-11-08 15:06:33 +0530

    volley

--- src/pages/data.js
+++
@@ -1,8 +0,0 @@
-// const pkg=[{
-//     id:1,
-//     name:"Volley",
-//     setup:`implementation("com.android.volley:volley:1.1.1")`,
-//     reference:"https://www.geeksforgeeks.org/making-api-calls-using-volley-library-in-android/",
-//     origin:"https://google.github.io/volley/"
-// }]
-// export default pkg;
