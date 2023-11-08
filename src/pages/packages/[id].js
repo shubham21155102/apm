0445671b53e5e5381588b91df40178651d0bec1e
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-11-08 15:07:34 +0530

    volley

--- src/pages/packages/[id].js
+++ src/pages/packages/[id].js
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import { useRouter } from 'next/router'
-import pkg from '../data'
 const Index = () => {
     const router = useRouter()
     const [ids,setId]=React.useState(null)
